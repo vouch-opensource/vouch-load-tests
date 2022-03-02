c9d91684507d882265944e05776f3d69d44a325d
Bernard Labno <bernard.labno@vouch.io>
2022-03-02 15:24:45 +0100

    Reporters should log errors

--- dev/com/example/io/reporter.clj
+++ dev/com/example/io/reporter.clj
@@ -6,27 +6,32 @@
     [clojure.tools.logging :as log]))
 
 (defn create-csv-reporter
-  [log-file-name]
-  (let [reporting (chan)]
-    (go
-      (with-open [writer (io/writer log-file-name)]
-        (csv/write-csv writer
-          [["method" "duration" "executor"]])
-        (loop []
-          (when-let [{:keys          [duration executor]
-                      {:keys [task]} :task} (<! reporting)]
-            (csv/write-csv writer
-              [[task duration executor]])
-            (recur)))))
-    reporting))
+  ([log-file-name] (create-csv-reporter log-file-name false))
+  ([log-file-name errors-to-console?]
+   (let [reporting (chan)]
+     (go
+       (with-open [writer (io/writer log-file-name)]
+         (csv/write-csv writer
+           [["method" "duration" "executor" "status"]])
+         (loop []
+           (when-let [{:keys          [duration executor error]
+                       {:keys [task]} :task} (<! reporting)]
+             (csv/write-csv writer
+               [[task duration executor (ex-message error)]])
+             (when (and errors-to-console? error)
+               (log/error (:id executor) "Failed to handle task" task error))
+             (recur)))))
+     reporting)))
 
 (defn create-log-reporter
   []
   (let [reporting (chan)]
     (go
       (loop []
-        (when-let [{:keys          [duration executor]
+        (when-let [{:keys          [duration executor error]
                     {:keys [task]} :task} (<! reporting)]
-          (log/info (format "%s took %dms %s" task duration executor))
+          (if error
+            (log/error executor "Failed to handle task" task error)
+            (log/info (format "%s took %dms %s" task duration executor)))
           (recur))))
     reporting))
